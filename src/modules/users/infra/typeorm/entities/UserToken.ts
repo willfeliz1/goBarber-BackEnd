5959c43285ecc1bd90bf8b7b62e9e0a57058fa3a
willfeliz1 <william_mfelizardo@hotmail.com>
2020-07-03 16:46:22 -0300

    feat: new entity Token to user

---
+++ src/modules/users/infra/typeorm/entities/UserToken.ts
@@ -0,0 +1,30 @@
+import { uuid } from 'uuidv4';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Generated,
+} from 'typeorm';
+
+@Entity('users_token')
+class UserToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default UserToken;
