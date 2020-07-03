da0b12e96d153cfda311a569d378e8111ab4618b
willfeliz1 <william_mfelizardo@hotmail.com>
2020-07-03 14:47:13 -0300

    Feat: checking if email user exists

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,8 @@
 
 import { injectable, inject } from 'tsyringe';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import { check } from 'prettier';
+import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
 // import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -20,6 +22,12 @@ class SendForgotPasswordEmailService {
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (!checkUserExists) {
+      throw new AppError('User does not exists.');
+    }
+
     this.mailProvider.sendMail(
       email,
       'Pedido de recuperação de senha recebido.',
