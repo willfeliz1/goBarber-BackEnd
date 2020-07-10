ce163d7811c700dadd080795b48cd39f2369e4c3
willfeliz1 <william_mfelizardo@hotmail.com>
2020-07-10 18:42:46 -0300

    feat: calling method with params to return date of provider

---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,43 @@
+import 'reflect-metadata';
+
+import { injectable, inject } from 'tsyringe';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllinMonthFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+      },
+    );
+
+    console.log(appointments);
+
+    return [{ day: 1, available: false }];
+  }
+}
+
+export default ListProviderMonthAvailabilityService;
