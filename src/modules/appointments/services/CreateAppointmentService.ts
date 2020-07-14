fc19d9651b9586a1fb89542f6b6977a4e1d4c907
willfeliz1 <william_mfelizardo@hotmail.com>
2020-07-14 18:30:04 -0300

    feat: validating if is the same user and creating appointments out of work hours

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour, isBefore } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -27,7 +27,17 @@ class CreateAppointmentService {
     const appointmentDate = startOfHour(date);
 
     if (isBefore(appointmentDate, Date.now())) {
-      throw new AppError("You can't create a appointment on a past date.");
+      throw new AppError("You can't create an appointment on a past date.");
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError("You can't create an appointment with yourserlf");
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError(
+        'you can only create appointments between 8am and 5pm ',
+      );
     }
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
