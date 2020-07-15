5f3ded0617b401a356e50294c3487b2a8d9a1c1a
willfeliz1 <william_mfelizardo@hotmail.com>
2020-07-15 15:22:35 -0300

    feat: listing of your appointments separated by days

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointsmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsConstroller';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointsmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', appointmentsController.create);
+
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
