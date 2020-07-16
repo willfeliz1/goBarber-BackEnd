2c11c6962aebe9a0d0c60acec022d561cbc4038b
willfeliz1 <william_mfelizardo@hotmail.com>
2020-07-16 14:09:49 -0300

    feat: validating enter requests body and param

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointsmentsController from '../controllers/AppointmentsController';
 import ProviderAppointmentsController from '../controllers/ProviderAppointmentsConstroller';
@@ -9,7 +11,16 @@ const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required,
+      date: Joi.date(),
+    },
+  }),
+  appointmentsController.create,
+);
 
 appointmentsRouter.get('/me', providerAppointmentsController.index);
 
