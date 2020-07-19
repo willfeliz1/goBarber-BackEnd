42ab50ae4e251eb02945f6524e5193232ab2ad53
willfeliz1 <william_mfelizardo@hotmail.com>
2020-07-19 12:57:24 -0300

    feat: mapping cacheProvider to instance methods

---
+++ src/shared/container/providers/CacheProvider/index.ts
@@ -0,0 +1,11 @@
+// mapeamento entre os provider que tenho
+
+import { container } from 'tsyringe';
+import RedisCacheProvider from './implementations/RedisCacheProvider';
+import ICacheProvider from './models/ICacheProvider';
+
+const providers = {
+  redis: RedisCacheProvider,
+};
+
+container.registerSingleton<ICacheProvider>('CacheProvider', providers.redis);
